6d1e61c26491720e8a801897f3e94bf13f5872c9
above <160554522+Abovescore@users.noreply.github.com>
2025-03-03 01:13:03 -0600

    Create config.js

---
+++ apps/proxy/uv/config.js
@@ -0,0 +1,11 @@
+self.__uv$config = {
+  prefix: "/apps/uv/",
+  bare: "https://goto.zkit.above.gay/bare/",
+  encodeUrl: Ultraviolet.codec.base64.encode,
+  decodeUrl: Ultraviolet.codec.base64.decode,
+  handler: "/uv/handler.js",
+  client: "/uv/client.js",
+  bundle: "/uv/bundle.js",
+  config: "/uv/config.js",
+  sw: "/uv/rizz.sw.js",
+};
